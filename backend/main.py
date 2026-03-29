7fe2f775f4d25a2fd542e679c3231bc0336d17a6
Lionel Yu <support@musicalbasics.com>
2026-03-28 18:55:20 -0700

    fix(backend): Add missing json import

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 import uvicorn
 import logging
+import json
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
